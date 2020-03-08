cf754e7fa7be444d4e6b4d4f00ba0947092fd61a
Ricky Adriell Cultura <pansoy.rickyadriell26457@gmail.com>
2020-03-08 11:09:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,7 @@ def changing_alan
   programmer_hash[:alan_kay][:known_for] = "GUI"
   programmer_hash
 end
-puts changing_alan
+#puts changing_alan
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
   # return the entire updated hash
